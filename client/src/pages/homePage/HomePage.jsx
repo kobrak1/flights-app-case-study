d235f957c63d3be3978f278d43c91f3d053406d4
kobrak1 <burakkarhan657@gmail.com>
2024-09-21 23:23:25 +0300

    modified: HomePage component has been modified by adding the Header component and its styles

--- client/src/pages/homePage/HomePage.jsx
+++ client/src/pages/homePage/HomePage.jsx
@@ -1,9 +1,11 @@
 import SearchBar from '../../components/searchBar/SearchBar'
+import Header from '../../components/header/Header';
 import "./HomePage.scss";
 
 const HomePage = () => {
   return (
     <div className='home-page'>
+      <Header />
       <SearchBar />
     </div>
   )
