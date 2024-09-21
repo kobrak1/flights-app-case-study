8de9ceb5d5931d48ce925e173b8ba072fe2b60e7
kobrak1 <burakkarhan657@gmail.com>
2024-09-21 23:21:01 +0300

    modified: Navbar has been fixed to the top

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import "./Navbar.scss";
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className="navbar">
       <div className="wrapper">
         <div className="left">
           <Link to="/" className="logo">
