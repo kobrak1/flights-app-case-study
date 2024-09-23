006536eac5ed8badc54f6af72d31d5e131745386
kobrak1 <burakkarhan657@gmail.com>
2024-09-23 23:37:45 +0300

    modified: modified

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import PromisePolyfill from 'promise-polyfill'
-import App from './App.jsx'
-import './index.scss'
+import { createRoot } from "react-dom/client";
+import { FlightProvider } from "./context/FlightContext.jsx";
+import PromisePolyfill from "promise-polyfill";
+import App from "./App.jsx";
+import "./index.scss";
 
 if (!window.Promise) {
-  window.Promise = PromisePolyfill
+  window.Promise = PromisePolyfill;
 }
 
-createRoot(document.getElementById('root')).render(<App />)
+createRoot(document.getElementById("root")).render(
+  <FlightProvider>
+    <App />
+  </FlightProvider>
+);
