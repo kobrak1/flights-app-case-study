260178a1e7a3ce7e0493b314c121ce0487b73d25
kobrak1 <burakkarhan657@gmail.com>
2024-09-23 23:40:08 +0300

    modified: new props have been imported to SearchBar component and responsive styles have been added in md size

--- client/src/components/searchBar/SearchBar.jsx
+++ client/src/components/searchBar/SearchBar.jsx
@@ -1,11 +1,33 @@
+import { memo } from "react";
+import { useFlight } from "../..//hooks/useFlight";
 import InputGroup from "../inputGroup/InputGroup";
-import { useState } from "react";
 import "./SearchBar.scss";
 
-const SearchBar = () => {
-  const [isRoundTrip, setIsRoundTrip] = useState(false);
+const SearchBar = memo(() => {
+  
+  const {
+    isRoundTrip,
+    departure,
+    destination,
+    dateTo,
+    dateFrom,
+    totalPassengers,
+    setIsRoundTrip,
+    handleDepartureChange,
+    handleDestinationChange,
+    handleDateToChange,
+    handleDateFromChange,
+    handlePassengers,
+    handleSwapper,
+    handleSubmit,
+  } = useFlight();
 
-  console.log("IsRoundTrip:", isRoundTrip);
+  //console.log("IsRoundTrip:", isRoundTrip);
+  //console.log("Departure:", departure);
+  //console.log("Destination:", destination);
+  //console.log("DateTo:", dateTo);
+  //console.log('DateFrom:', dateFrom);
+  //console.log('Passengers:', totalPassengers);
 
   return (
     <div className="search-bar">
@@ -28,16 +50,18 @@ const SearchBar = () => {
           <span>Çift yön</span>
         </label>
       </form>
-      <form className="flight-details">
-        <div className="place-inputs">
+      <form className="flight-form" onSubmit={handleSubmit}>
+        <div className="search-fields">
           <InputGroup
             type="text"
             htmlFor="departure"
             label="Nereden"
+            handleChange={handleDepartureChange}
+            value={departure}
             placeholder="Şehir veya havalimanı"
           />
           <div className="flight-swapper">
-            <button>
+            <button onClick={handleSwapper}>
               <img src="/icons/swap.png" alt="swap icon not found" />
             </button>
           </div>
@@ -45,31 +69,41 @@ const SearchBar = () => {
             type="text"
             htmlFor="destination"
             label="Nereye"
+            handleChange={handleDestinationChange}
+            value={destination}
             placeholder="Şehir veya havalimanı"
           />
           <InputGroup
             type="date"
             htmlFor="date1"
             label="Gidiş"
+            handleChange={handleDateToChange}
+            value={dateTo}
             placeholder=""
           />
           <InputGroup
             type="date"
-            htmlFor="date1"
+            htmlFor="date2"
             label="Dönüş"
+            handleChange={handleDateFromChange}
+            value={dateFrom}
+            disabled={!isRoundTrip}
             placeholder=""
           />
           <div className="passenger-count">
             <label htmlFor="departure">Yolcu sayısı</label>
-            <span>1 Yolcu / Ekonomi</span>
+            <span>{totalPassengers} Yolcu / Ekonomi</span>
           </div>
         </div>
-        <div className="button">
-          <button className="search-button">Bilet bul</button>
-        </div>
+        <button type="submit" className="search-button">
+          Bilet bul
+        </button>
       </form>
     </div>
   );
-};
+});
+
+// Set a display name for better debugging
+SearchBar.displayName = "SearchBar";
 
 export default SearchBar;
