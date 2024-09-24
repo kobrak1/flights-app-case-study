31a1fa393781a479d990d21c0fea3ac441f5c5a9
kobrak1 <burakkarhan657@gmail.com>
2024-09-24 16:46:57 +0300

    add: Sort button have been added

---
+++ client/src/components/sortField/SortField.jsx
@@ -0,0 +1,13 @@
+import "./SortField.scss"
+
+const SortField = () => {
+  return (
+    <div className="sort-buttons">
+      <button className="price">Fiyat</button>
+      <button className="time">Kalkış saati</button>
+      <button className="duration">Uçuş süresi</button>
+    </div>
+  )
+}
+
+export default SortField
