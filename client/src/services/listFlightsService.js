08a9d3d6331c60a1a1996446c24c940860ae4124
kobrak1 <burakkarhan657@gmail.com>
2024-09-26 13:39:47 +0300

    modified: unnecessary axios request and response interceptors have been removed to reduce request and response times

--- client/src/services/listFlightsService.js
+++ client/src/services/listFlightsService.js
@@ -1,33 +1,17 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://api.schiphol.nl/public-flights/flights?includedelays=false&page=0&sort=%2BscheduleTime',
-    timeout: 5000,
-    headers: {
-        "resourceversion": "v4",
-        "app_id": "9f7e1173",
-        "app_key": "8216a1026e6600c0b104e66cbbce6ac7",
-        "Accept": "application/json",
+  baseURL: "https://flights-app-case-study.onrender.com",
+  timeout: 5000,
+});
+
+export const fetchFlights = async ({departure, destination, dateTo}) => {
+  const response = await api.get("/api/fetch-flights", {
+    params: {
+      flightDirection: "D",
+      scheduleDate: dateTo,
+      'route[destinations][0]': `${destination}`,
     }
-})
-
-
-export const listFlights = async ({ departure, destination, dateFrom, dateTo }) => {
-    const params = new URLSearchParams({
-        includedelays: false,
-        page: 0,
-        sort: "+scheduleTime",
-        departure: departure,
-        destination: destination,
-        dateFrom: dateFrom,  // Format as needed (e.g., YYYY-MM-DD)
-        dateTo: dateTo,      // Format as needed (e.g., YYYY-MM-DD)
-    });
-
-    try {
-        const response = await api.get(`/?${params}`);
-        return response;
-    } catch (error) {
-        console.error('Error fetching flights:', error);
-        throw error;
-    }
+  })
+  return response.data;
 };
