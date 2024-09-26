f9e7c27f73f3280cee4c47f88206a5c5d9cc6471
kobrak1 <burakkarhan657@gmail.com>
2024-09-26 13:35:59 +0300

    modified: service names have been changed on useServices.jsx

--- client/src/hooks/useServices.jsx
+++ client/src/hooks/useServices.jsx
@@ -3,80 +3,80 @@ import { addFlight, fetchFlightById, fetchFlights, updateFlight, deleteFlight } 
 import { message } from "antd"
 
 // GET
-export const useNotes = () => {
+export const useFlights = () => {
     return useQuery({
-        queryKey: ['notes'],
+        queryKey: ['flights'],
         queryFn: fetchFlights,
         staleTime: 1000 * 60 * 5,  // data remains fresh for 5 minutes
         cacheTime: 1000 * 60 * 10, // cache data for 10 minutes
         onError: (error) => {
-            console.error('Error fetching notes:', error);
-            message.error('Failed to fetch notes.');
+            console.error('Error fetching flights:', error);
+            message.error('Failed to fetch flights.');
         },
     })
 }
 
 // GET/:id
-export const useNote = (id) => {
+export const useFlight = (id) => {
     return useQuery({
-        queryKey: ['note', id],
+        queryKey: ['flight', id],
         queryFn: () => fetchFlightById(id),
         staleTime: 1000 * 60 * 5,  // data remains fresh for 5 minutes
         cacheTime: 1000 * 60 * 10, // cache data for 10 minutes
         onError: (error) => {
-            console.error('Error fetching the note:', error);
-            message.error('Failed to fetch the note.');
+            console.error('Error fetching the flight:', error);
+            message.error('Failed to fetch the flight.');
         },
     })
 }
 
 // POST
-export const useCreateNote = () => {
+export const useCreateFlight = () => {
     const queryClient = useQueryClient()
 
     return useMutation({
         mutationFn: data => addFlight(data),
         onSuccess: () => {
-            queryClient.invalidateQueries(['notes'])
+            queryClient.invalidateQueries(['flights'])
             message.success('Created successfully')
         },
         onError: (error) => {
-            console.error('Error creating note:', error)
-            message.error('Error adding note')
+            console.error('Error creating flight:', error)
+            message.error('Error adding flight')
         },
     })
 }
 
 // PUT
-export const useUpdateNote = () => {
+export const useUpdateFlight = () => {
     const queryClient = useQueryClient()
 
     return useMutation({
         mutationFn: ({ id, important }) => updateFlight(id, {important}),
         onSuccess: () => {
-            queryClient.invalidateQueries(['notes'])
+            queryClient.invalidateQueries(['flights'])
             message.success('Updated successfully')
         },
         onError: (error) => {
-            console.error('Error updating note:', error)
-            message.error('Error updating note')
+            console.error('Error updating flight:', error)
+            message.error('Error updating flight')
         }
     })
 }
 
 // DELETE
-export const useDeleteNote = () =>{
+export const useDeleteFlight = () =>{
     const queryClient = useQueryClient()
 
     return useMutation({
         mutationFn: id => deleteFlight(id),
         onSuccess: () => {
-            queryClient.invalidateQueries(['notes'])
+            queryClient.invalidateQueries(['flights'])
             message.success('Removed successfully')
         },
         onError: (error) => {
-            console.error('Error deleting note:', error)
-            message.error('Error deleting note')
+            console.error('Error deleting flight:', error)
+            message.error('Error deleting flight')
         }
     })
 }
