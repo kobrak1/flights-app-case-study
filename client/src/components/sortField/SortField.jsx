2352387e15ef66039175ee700acf1b02d680cba8
kobrak1 <burakkarhan657@gmail.com>
2024-09-27 15:20:41 +0300

    modified: SortFieldnow contains scheduleDate

--- client/src/components/sortField/SortField.jsx
+++ client/src/components/sortField/SortField.jsx
@@ -1,13 +1,18 @@
-import "./SortField.scss"
+import "./SortField.scss";
 
-const SortField = () => {
+const SortField = ({dateTo}) => {
   return (
-    <div className="sort-buttons">
-      <button className="price">Fiyat</button>
-      <button className="time">Kalkış saati</button>
-      <button className="duration">Uçuş süresi</button>
+    <div className="main-container">
+      <div className="sort-buttons">
+        <button className="price">Fiyat</button>
+        <button className="time">Kalkış saati</button>
+        <button className="duration">Uçuş süresi</button>
+      </div>
+      <div className="date">
+        <span>{dateTo}</span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SortField
+export default SortField;
